da13228c95100b887acdd70fb02645f6a00fa6d4
liam barnes <john.ragun7@outlook.com>
2019-04-04 07:45:57 +0000

    rotate encrypt/decrypt put into functions

--- main.c
+++ main.c
@@ -1,6 +1,8 @@
 #include<stdio.h>
 
-//functions: encrypt rotation cipher, decrypt rotation cipher, encrypt substitution cipher, decrypt substitution cipher, decrypt rotation cipher CIPHER TEXT ONLY, decrypt substitution cipher CIPHER TEXT ONLY
+//functions: encrypt substitution cipher, decrypt substitution cipher, decrypt rotation cipher CIPHER TEXT ONLY, decrypt substitution cipher CIPHER TEXT ONLY
+void rotate_encrypt (char x[], int k);
+void rotate_decrypt (char x[], int k);
 
 int main() {
     char x[1000]; //array to hold string
@@ -8,9 +10,8 @@ int main() {
     //sprintf(x, "ABCDEFGHIJKLMNOPQRSTUVWXYZ"); //prints the phrase to be encoded to the array "x"  above
     sprintf(x, "HEROES ARE IMPORTANT. HEROES TELL US WHO WE WANT TO BE BUT WHEN THEY MADE THIS PARTICULAR HERO THEY DIDN'T GIVE HIM A GUN, THEY GAVE HIM A SCREWDRIVER TO FIX THINGS. THEY DIDN'T GIVE HIM A TANK OR A WARSHIP OR AN X-WING, THEY GAVE HIM A CALL BOX FROM WHICH YOU CAN CALL FOR HELP AND THEY DIDN'T GIVE HIM A SUPERPOWER OR A HEAT RAY, THEY GAVE HIM AN EXTRA HEART. AND THAT'S EXTRAORDINARY. THERE WILL NEVER COME A TIME WHEN WE DON'T NEED A HERO LIKE THE DOCTOR."); //prints the phrase to be encoded to the array "x"  above
     
-    int y=0; //loop counter
-    
     //this loop will convert any inputs that are lower case letters to upper case letters
+    int y=0; //loop counter
     while(x[y]!=NULL) { //loop that will continue until the end of the string
         if(x[y]<96 || x[y]>123) { //ensures only lower case letters are having their ascii changed
             y++; //increments loop counter
@@ -21,18 +22,29 @@ int main() {
         }
     }
     
+    int k=-15;
     
+    rotate_encrypt (x, k);
+    
+    k=k-(k*2); //changes encryption key to decode phrase
+    
+    rotate_decrypt(x, k);
+    
+    
+    
+    return 0; 
+}
 
-    
-    int k=-15; //encryption key
-    
-    
+
+
+
+void rotate_encrypt (char x[], int k) {
     //this loop will encrypt a rotation cipher
     if(k<26 && k>-26) { //ensures encryption key is valid
-        y=0; //resets loop counter
+        int y=0; //loop counter
         while(x[y]!=NULL) { //loop that will continue until the end of the string
             if(x[y]>64 && x[y]<91) { //ensures only letters are having their ascii changed
-//                printf("%d -> ", x[y]); //prints the letters ascii value e.g A prints 65
+//                printf( "%d -> ", x[y]); //prints the letters ascii value e.g A prints 65
                 x[y]=x[y]+k;  //encodes the letter by shunting it according to the encryption key (k)
                 if(x[y]<65) { //if encryption shunts to below of letter range, this will shunt to end of letter range
                     x[y]=x[y]+26;
@@ -53,16 +65,16 @@ int main() {
     else { //if encryption key isnt valid, prints error message
         printf("error: invalid encryption key\n");
     }
+}
 
 
 
 
-    k=k-(k*2); //changes encryption key to decode phrase
-    
-    
+
+void rotate_decrypt (char x[], int k) {
     //this loop will decrypt a rotation cipher
     if(k<26 && k>-26) { //ensures encryption key is valid
-        y=0; //resets loop couner
+        int y=0; //loop couner
         while(x[y]!=NULL) {  //loop that will continue until the end of the string
             if(x[y]>64 && x[y]<91) {  //ensures only letters are having their ascii changed
 //                printf("%d -> ", x[y]);  //prints the letters ascii value e.g A prints 65
@@ -86,11 +98,4 @@ int main() {
     else { //if encryption key isnt valid, prints error message
         printf("error: invalid decryption key\n");
     }
-    
-    
-    
-    
-    
-    
-    return 0; 
 }
