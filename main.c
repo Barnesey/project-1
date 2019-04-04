93b9cbd01c617b06e0d6df66b6963c8cd66712bc
liam barnes <john.ragun7@outlook.com>
2019-04-04 03:46:07 +0000

    moffat

--- main.c
+++ main.c
@@ -3,9 +3,10 @@
 //functions: encrypt rotation cipher, decrypt rotation cipher, encrypt substitution cipher, decrypt substitution cipher, decrypt rotation cipher CIPHER TEXT ONLY, decrypt substitution cipher CIPHER TEXT ONLY
 
 int main() {
-    char x[100]; //array to hold string
-    sprintf(x, "ATTACK AT DAWN"); //prints the phrase to be encoded to the array "x"  above
+    char x[1000]; //array to hold string
+    //sprintf(x, "ATTACK AT DAWN"); //prints the phrase to be encoded to the array "x"  above
     //sprintf(x, "ABCDEFGHIJKLMNOPQRSTUVWXYZ"); //prints the phrase to be encoded to the array "x"  above
+    sprintf(x, "HEROES ARE IMPORTANT. HEROES TELL US WHO WE WANT TO BE BUT WHEN THEY MADE THIS PARTICULAR HERO THEY DIDN'T GIVE HIM A GUN, THEY GAVE HIM A SCREWDRIVER TO FIX THINGS. THEY DIDN'T GIVE HIM A TANK OR A WARSHIP OR AN X-WING, THEY GAVE HIM A CALL BOX FROM WHICH YOU CAN CALL FOR HELP AND THEY DIDN'T GIVE HIM A SUPERPOWER OR A HEAT RAY, THEY GAVE HIM AN EXTRA HEART. AND THAT'S EXTRAORDINARY. THERE WILL NEVER COME A TIME WHEN WE DON'T NEED A HERO LIKE THE DOCTOR."); //prints the phrase to be encoded to the array "x"  above
     
     int y=0; //loop counter
     
@@ -31,7 +32,7 @@ int main() {
         y=0; //resets loop counter
         while(x[y]!=NULL) { //loop that will continue until the end of the string
             if(x[y]>64 && x[y]<91) { //ensures only letters are having their ascii changed
-                printf("%d -> ", x[y]); //prints the letters ascii value e.g A prints 65
+//                printf("%d -> ", x[y]); //prints the letters ascii value e.g A prints 65
                 x[y]=x[y]+k;  //encodes the letter by shunting it according to the encryption key (k)
                 if(x[y]<65) { //if encryption shunts to below of letter range, this will shunt to end of letter range
                     x[y]=x[y]+26;
@@ -39,11 +40,11 @@ int main() {
                 if(x[y]>90) { //if encryption shunts to above of letter range, this will shunt to beginning of letter range
                     x[y]=x[y]-26;
                 }
-                printf("%d\n", x[y]);  //prints new ascii value 
+//                printf("%d\n", x[y]);  //prints new ascii value 
                 y++;  //increments loop counter to encode the next letter
             }
             else {  //if ascii value is not for a letter, this will print it unchanged
-                printf("%d\n", x[y]);
+//                printf("%d\n", x[y]);
                 y++;
             }
         }
@@ -64,7 +65,7 @@ int main() {
         y=0; //resets loop couner
         while(x[y]!=NULL) {  //loop that will continue until the end of the string
             if(x[y]>64 && x[y]<91) {  //ensures only letters are having their ascii changed
-                printf("%d -> ", x[y]);  //prints the letters ascii value e.g A prints 65
+//                printf("%d -> ", x[y]);  //prints the letters ascii value e.g A prints 65
                 x[y]=x[y]+k;   //decodes the letter by shunting it according to the decryption key (k)
                 if(x[y]>90) { //if encryption shunts to above of letter range, this will shunt to beginning of letter range
                     x[y]=x[y]-26;
@@ -72,11 +73,11 @@ int main() {
                 if(x[y]<65) { //if decryption shunts to below of letter range, this will shunt to end of letter range
                     x[y]=x[y]+26;
                 }
-                printf("%d\n", x[y]);   //prints new ascii value 
+//                printf("%d\n", x[y]);   //prints new ascii value 
                 y++;   //increments loop counter to decode the next letter
             }
             else {  //if ascii value is not for a letter, this will print it unchanged
-                printf("%d\n", x[y]);
+//                printf("%d\n", x[y]);
                 y++; //increments loop counter to decode next letter
             }
         }
